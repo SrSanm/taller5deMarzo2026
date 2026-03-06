0ecc08124d5d9f7fb868983725822994fed7c2d4
andres felipe sanmartin valencia <andres.sanmartin@correo.tdea.edu.co>
2026-03-05 19:38:22 -0500

    historia por andres felipe sanmartin valencia

---
+++ index.js
@@ -0,0 +1,14 @@
+function historia(){
+    do{
+        let listo = confirm ("¿estas listo para la historia?");
+        if (listo === true){
+            console.log("cuando estaba pequeño mi abula nos cuidaba a mi hermana y a mi, un dia mi abuela prendio incienso con unas matas que para limpiar la casa")
+            console.log("mi hermana y yo nos ibamos pegados a ella mirando que hacia por curiosidad, mi abuela dejo en el piso la olla donde hacia eso")
+            console.log("mi hermana le dio por ver, le parecio llamativo y decidio meter el pie en, se termino quemando, mi abuela llamo a mi papá que es farmaceutico")
+            console.log("le puso unas cremas y la llevo a urgencias, todo salio bien y ahora tengo una buena historia romper el hielo :)")
+            return;
+        }
+
+    }while(true)
+}
+historia();
